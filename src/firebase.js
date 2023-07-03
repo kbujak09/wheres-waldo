61a1a9a5e5fe3241cf6eed9711fdc4e976694746
Kacper Bujak <kaccpper2@gmail.com>
2023-07-03 13:35:25 +0200

    changes firebase config for gh pages

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,20 @@ import { getFirestore } from "@firebase/firestore";
 import { doc, getDoc, getDocs, collection } from '@firebase/firestore' 
 
 const firebaseConfig = {
-  apiKey: `${process.env.REACT_APP_API_KEY}`,
-  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
-  projectId: `wheres-waldo-93a2a`,
-  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
-  messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
-  appId: `${process.env.REACT_APP_ID}`,
-  measurementId: `${process.env.REACT_APP_MEASUREMENT_ID}`
+  // apiKey: `${process.env.REACT_APP_API_KEY}`,
+  // authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+  // projectId: `wheres-waldo-93a2a`,
+  // storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+  // messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
+  // appId: `${process.env.REACT_APP_ID}`,
+  // measurementId: `${process.env.REACT_APP_MEASUREMENT_ID}`
+  apiKey: "AIzaSyBzAIGe_zfOB6lC1IzgwSfKr9tGryRXOPs",
+  authDomain: "wheres-waldo-93a2a.firebaseapp.com",
+  projectId: "wheres-waldo-93a2a",
+  storageBucket: "wheres-waldo-93a2a.appspot.com",
+  messagingSenderId: "114176723187",
+  appId: "1:114176723187:web:bca52b5ee9571486ce4f86",
+  measurementId: "G-T23W12R789"
 };
 
 // Initialize Firebase
